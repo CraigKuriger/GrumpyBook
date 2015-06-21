af8ef7d8b592fa3b81085437f789ab5ee6a92ba0
craigkuriger <craig.kuriger@xtra.co.nz>
2015-06-21 17:27:10 +1200

    sorted signing out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-devise_for :users, controllers: {
-        sessions: 'sessions'
-      }
+
+devise_for :users, controllers: { registrations: "users/registrations" }
+
   resources :statuses
   root to: 'statuses#index'
 
