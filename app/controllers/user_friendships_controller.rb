395e221cee4dae4b5641a22695c2b99f133aa888
CraigKuriger <craig.kuriger@icloud.com>
2015-07-25 12:20:15 +1200

    add friends updated

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -10,7 +10,7 @@ class UserFriendshipsController < ApplicationController
 
 	def accept
 		@user_friendship = current_user.user_friendships.find(params[:id])
-		if @user_friendship.accept
+		if @user_friendship.accept!
 			flash[:success] = "You are now connected to #{@user_friendship.friend.first_name}"
 		else
 			flash[:error] = "Declined"
